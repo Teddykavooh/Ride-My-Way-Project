4b22c1c21890157166102b796d1be4341b7cf51e
Teddykavooh <teddykavooh@gmail.com>
2018-06-29 01:15:38 +0300

    Tokens Implemented

--- app/__init__.py
+++ app/__init__.py
@@ -36,25 +36,14 @@ def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
-        # read connection parameters
         parameters = config()
-
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**parameters)
-
-        # create a cursor
         cur = conn.cursor()
-
-        # execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-
-        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
