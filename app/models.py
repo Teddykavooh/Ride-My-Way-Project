775bfb6db584d5ba0b8d93a56e04e1978112d0bd
Teddykavooh <teddykavooh@gmail.com>
2018-08-24 11:18:34 +0300

    Data Structures successfully and efficiently restructured.

--- app/models.py
+++ app/models.py
@@ -78,17 +78,6 @@ class Rides:
             all_rides["route"] = my_data2
             all_rides["time"] = my_data3
             the_list.append(all_rides)
-            # the_list.append(my_data)
-            # the_list.append(my_data1)
-            # the_list.append(my_data2)
-            # the_list.append(my_data3)
-            # all_rides[my_data] = {"driver": ride[1], "route": ride[2], "time": ride[3]}
-        # if all_rides == {}:
-        #     return {"txt": "No rides are available"}
-        # else:
-        #     conn.commit()
-        #     cur.close()
-        #     return all_rides
         if the_list is None:
             return {"txt": "No rides are available"}
         else:
@@ -100,18 +89,11 @@ class Rides:
         conn = psycopg2.connect(database=os.getenv('Db'), host=os.getenv('host'), user=os.getenv('user'),
                                 password=os.getenv('password'))
         cur = conn.cursor()
-        cur.execute("SELECT * FROM rides")
-        rides = cur.fetchall()
-        all_rides = {}
-        for ride in rides:
-            my_data = ride[0]
-            all_rides[my_data] = {"driver": ride[1], "route": ride[2], "time": ride[3]}
-        if ride_id in all_rides:
-            conn.commit()
-            cur.close()
-            return all_rides
-        else:
+        cur.execute("SELECT * from rides where ride_id='{}'".format(ride_id))
+        row = cur.fetchone()
+        if row is None:
             return {"txt": "Ride not available"}
+        return {"ride_id": row[0], "driver": row[1], "route": row[2], "time": row[3]}
 
     def post_a_ride(self, driver, route, time):
         conn = psycopg2.connect(database=os.getenv('Db'), host=os.getenv('host'), user=os.getenv('user'),
@@ -128,38 +110,32 @@ class Rides:
         conn = psycopg2.connect(database=os.getenv('Db'), host=os.getenv('host'), user=os.getenv('user'),
                                 password=os.getenv('password'))
         cur = conn.cursor()
-        cur.execute("SELECT * FROM rides")
-        rides = cur.fetchall()
-        all_rides = {}
-        for ride in rides:
-            my_data = ride[0]
-            all_rides[my_data] = {"driver": ride[1], "route": ride[2], "time": ride[3]}
-        if ride_id in all_rides:
+        # cur.execute("SELECT * FROM rides")
+        # rides = cur.fetchall()
+        cur.execute("SELECT * from rides where ride_id='{}'".format(ride_id))
+        row = cur.fetchone()
+        if row is None:
+            return {"txt": "Ride not available"}
+        else:
             cur.execute("DELETE FROM rides WHERE ride_id = %s", (ride_id,))
             conn.commit()
             cur.close()
             return {"txt": "Ride Deleted"}
-        else:
-            return {"txt": "Ride not available"}
 
     def edit(self, ride_id, driver, route, time):
         conn = psycopg2.connect(database=os.getenv('Db'), host=os.getenv('host'), user=os.getenv('user'),
                                 password=os.getenv('password'))
         cur = conn.cursor()
-        cur.execute("SELECT * FROM rides")
-        rides = cur.fetchall()
-        all_rides = {}
-        for ride in rides:
-            my_data = ride[0]
-            all_rides[my_data] = {"driver": ride[1], "route": ride[2], "time": ride[3]}
-        if ride_id in all_rides:
+        cur.execute("SELECT * from rides where ride_id='{}'".format(ride_id))
+        row = cur.fetchone()
+        if row is None:
+            return {"txt": "Ride not available"}
+        else:
             sql = """ UPDATE rides SET driver = %s, route = %s, time = %s WHERE ride_id = %s"""
             cur.execute(sql, (driver, route, time, ride_id))
             conn.commit()
             cur.close()
             return {"txt": "Ride Edited"}
-        else:
-            return {"txt": "Ride not available"}
 
     def request_to_join_a_ride(self, ride_id, passenger_name, pick_up_station, time):
         conn = psycopg2.connect(database=os.getenv('Db'), host=os.getenv('host'), user=os.getenv('user'),
@@ -167,14 +143,6 @@ class Rides:
         cur = conn.cursor()
         cur.execute("SELECT * from rides where ride_id='{}'".format(ride_id))
         row = cur.fetchone()
-        # all_rides = {}
-        # for ride in rides:
-        #     my_data = ride[0]
-        #     all_rides[my_data] = {"driver": ride[1], "route": ride[2], "time": ride[3]}
-        #     if my_data in all_rides:
-        #         return {"txt": "You can't request more rides"}
-        #     else:
-        # print(row)
         if row is None:
             return {"txt": "Ride does not exist"}
         else:
@@ -242,17 +210,29 @@ class Users:
                                 password=os.getenv('password'))
         cur = conn.cursor()
         cur.execute("SELECT * FROM users ORDER BY user_id")
-        users = cur.fetchall()
-        all_users = {}
-        for user in users:
-            my_data = user[0]
-            all_users[my_data] = {"username": user[1], "email": user[2], "password": user[3]}
-        if all_users == {}:
+        rows = cur.fetchall()
+        the_list = []
+        for ride in rows:
+            all_rides = {}
+            my_data = ride[0]
+            my_data1 = ride[1]
+            my_data2 = ride[2]
+            my_data3 = ride[3]
+            my_data4 = ride[4]
+            my_data5 = ride[5]
+            all_rides["user_id"] = my_data
+            all_rides["username"] = my_data1
+            all_rides["email"] = my_data2
+            all_rides["password"] = my_data3
+            all_rides["driver"] = my_data4
+            all_rides["admin"] = my_data5
+            the_list.append(all_rides)
+        if rows is None:
             return {"txt": "No users are available"}
         else:
             conn.commit()
             cur.close()
-            return all_users
+            return the_list
 
     def register(self, username, email, password, driver, admin):
         """Creates new user"""
